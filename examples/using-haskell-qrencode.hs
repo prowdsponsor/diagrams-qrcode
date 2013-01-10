4e89f9406991581aff250b7a23a8c4f3e335d7a3
Felipe Lessa <felipe.lessa@gmail.com>
2013-01-10 16:18:26 -0200

    Add example using haskell-qrencode library.

---
+++ examples/using-haskell-qrencode.hs
@@ -0,0 +1,20 @@
+import Text.Blaze.Svg.Renderer.Utf8 (renderSvg)   -- blaze-svg
+import qualified Data.ByteString.Lazy.Char8 as L8 -- bytestring
+import qualified Data.QRCode as QR                -- haskell-qrencode
+import qualified Diagrams.Backend.SVG as D        -- diagrams-svg
+import qualified Diagrams.Prelude as D            -- diagrams-lib
+import qualified Diagrams.QRCode as QR            -- diagrams-qrcode
+
+main :: IO ()
+main = do
+  -- Read input from stdin
+  input <- getContents
+
+  -- Calculate QR code
+  qrcode <- QR.encodeString input Nothing QR.QR_ECLEVEL_M QR.QR_MODE_EIGHT True
+
+  -- Draw QR code
+  let dia = QR.stroke $ D.scale 6 $ QR.pathMatrix $ QR.toMatrix qrcode
+
+  -- Render diagram
+  L8.putStrLn $ renderSvg $ D.renderDia D.SVG (D.SVGOptions D.Absolute) dia
